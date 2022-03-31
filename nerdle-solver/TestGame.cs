c036d312b124e47e0fa59332d1a795ca95644ccd
John Jermanis <jermanis@yahoo.com>
2022-03-31 11:54:53 -0700

    Optimization: run tests in parallel

--- nerdle-solver/TestGame.cs
+++ nerdle-solver/TestGame.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace nerdle_solver
 {
     public class TestGame
     {
         private const int GUESS_COUNT = 6;
+        private readonly bool SHOW_EQUATION_DETAILS = true;
+
         private IList<string> _allEquations;
 
         public TestGame()
@@ -18,14 +22,20 @@ namespace nerdle_solver
             int start = Environment.TickCount;
             Console.WriteLine($"Running test: {_allEquations.Count} equations");
             var results = new ResultDistribution(GUESS_COUNT);
-            foreach (var equation in _allEquations)
+            if (SHOW_EQUATION_DETAILS)
+                Console.WriteLine($"First guess: {_allEquations.First()}");
+            Parallel.ForEach(_allEquations, equation =>
             {
                 var score = PlayGame(equation);
                 if (score.HasValue)
                     results.ScoreCount[score.Value]++;
                 else
+                {
                     results.Misses++;
-            }
+                    if (SHOW_EQUATION_DETAILS)
+                        Console.WriteLine($"Missed: {equation}");
+                }
+            });
             Console.Write(results);
             Console.WriteLine($"Time: {Environment.TickCount - start} ms");
         }
